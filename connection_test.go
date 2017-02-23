f021c2b3057faf93cfdec6f539f652892df9c68b
Matt Bernhard <matber@umich.edu>
2017-02-23 18:46:16 -0500

    Added more connection tests

--- connection_test.go
+++ connection_test.go
@@ -8,14 +8,10 @@ var (
 	test_tube = "https://ct.googleapis.com/testtube"
 )
 
-func TestNew(t *testing.T) {
-	uri := test_tube
-	bucketSize := int64(1)
-
-	lSC := New(uri, bucketSize)
+func testLSC(lSC *LogServerConnection, bucketSize int64, offset int64, t *testing.T) {
 
 	if lSC != nil {
-		if lSC.start != 0 || lSC.end != bucketSize {
+		if lSC.start != offset || lSC.end != bucketSize+offset {
 			t.Errorf("Bucket indices are incorrect")
 			return
 		}
@@ -26,4 +22,67 @@ func TestNew(t *testing.T) {
 	}
 
 	return
+
+}
+
+func TestNew(t *testing.T) {
+	uri := test_tube
+	bucketSize := int64(1)
+
+	lSC := New(uri, bucketSize)
+
+	testLSC(lSC, bucketSize, int64(0), t)
+
+}
+
+func TestNewWithOffset0(t *testing.T) {
+	uri := test_tube
+	bucketSize := int64(1)
+	offset := int64(0)
+
+	lSC := NewWithOffset(uri, bucketSize, offset)
+
+	testLSC(lSC, bucketSize, offset, t)
+
+}
+
+func TestNewWithOffset1(t *testing.T) {
+	bucketSize := int64(1)
+	offset := int64(1)
+
+	lSC := NewWithOffset(test_tube, bucketSize, offset)
+
+	testLSC(lSC, bucketSize, offset, t)
+
+}
+
+func TestSlideBucket(t *testing.T) {
+	bucketSize := int64(10)
+
+	lSC := New(test_tube, bucketSize)
+
+	lSC.slideBucket()
+
+	if lSC.start != bucketSize+1 || lSC.end != bucketSize*2 {
+		t.Errorf("Couldn't slide bucket %d, %d", lSC.start, lSC.end)
+		return
+	}
+}
+
+func TestGetLogEntries(t *testing.T) {
+	bucketSize := int64(10)
+
+	lSC := New(test_tube, bucketSize)
+
+	entries, err := lSC.GetLogEntries()
+
+	if err != nil {
+		t.Errorf("Couldn't get log entries!")
+		return
+	}
+
+	if int64(len(entries)) != bucketSize+1 {
+		t.Errorf("Did not get the right number of entries: %d", len(entries))
+		return
+	}
 }
