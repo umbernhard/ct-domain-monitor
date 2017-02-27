7a82adef85020682527272a5364dd24f2c6bbadd
Matt Bernhard <matber@umich.edu>
2017-02-27 17:25:51 -0600

    Fleshing out API more

--- app/model.go
+++ app/model.go
@@ -9,11 +9,27 @@ import (
 
 type record struct {
 	Domain string `json:"name"`
-	Certs  []byte `json:"certs"`
+	Cert   string `json:"cert"`
 }
 
-func (r *record) getDomain(db *sql.DB) error {
-	return errors.New("Not implemented")
+func (r *record) getDomain(db *sql.DB) ([]string, error) {
+	rows, err := db.Query("SELECT domain, cert_pem FROM domains WHERE domain=$1", r.Domain)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	certs := make([]string, 0)
+
+	for rows.Next() {
+		var r record
+		if err := rows.Scan(&r.Domain, &r.Cert); err != nil {
+			return nil, err
+		}
+		certs = append(certs, r.Cert)
+	}
+
+	return certs, nil
 }
 
 func (r *record) updateDomain(db *sql.DB) error {
